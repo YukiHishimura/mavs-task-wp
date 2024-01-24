f491122cc0dfa2485a7e0accd4b112146939262a
YukiHishimura <154481841+YukiHishimura@users.noreply.github.com>
2024-01-25 00:29:06 +0900

    要素出現コード修正

--- themes/mavs-wp/assets/js/scroll-anime.js
+++ themes/mavs-wp/assets/js/scroll-anime.js
@@ -1,24 +1,19 @@
 // ----------スクロールで要素を出現させる--------------------
-const aboutSec = document.querySelector('#about');
-const serviceSec = document.querySelector('#service');
-const blogSec = document.querySelector('#blog');
-const memberSec = document.querySelector('#member');
-const recruitSec = document.querySelector('#recruit');
-const newsSec = document.querySelector('#news');
-
 const elems = document.querySelectorAll('.scrollTrigger');
 
 function showEl() {
     const scroll = window.scrollY;
+    const bodyHeight = document.body.clientHeight;
     const windowHeight = window.innerHeight;
+    const bottomPoint = bodyHeight - windowHeight;
+
 
     elems.forEach(function(elem, i) {
         let elemPos = elem.getBoundingClientRect().top + scroll;
 
-        if (windowHeight * 0.6 + scroll >= elemPos) {
+        if (windowHeight * 0.6 + scroll >= elemPos ||
+            scroll >= bottomPoint) {
             elem.classList.add('show');
-        // } else {
-        //     elem.classList.remove('show');
         }
     });
 }
