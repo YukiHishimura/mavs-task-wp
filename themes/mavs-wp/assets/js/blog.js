3ac6daf50f0d06f2a206ef3168e086a86f227cbd
YukiHishimura <154481841+YukiHishimura@users.noreply.github.com>
2024-01-26 19:16:04 +0900

    カテゴリリスト展開（SP）追加

---
+++ themes/mavs-wp/assets/js/blog.js
@@ -0,0 +1,22 @@
+// ----------SP ボタンクリックでカテゴリー展開--------------------
+const catEl = document.querySelector('#category');
+const catBtn = document.querySelector('#category__btn');
+let listOpened = false;
+
+catBtn.addEventListener('click', function() {
+    this.style.pointerEvents = "none";
+
+    if (listOpened) {
+        catEl.classList.remove('opened');
+        catBtn.classList.remove('opened');
+        listOpened = false;
+    } else {
+        catEl.classList.add('opened');
+        catBtn.classList.add('opened');
+        listOpened = true;
+    }
+
+    setTimeout(function() {
+        catBtn.style.pointerEvents = "auto";
+    }, 400);
+});
